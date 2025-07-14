2b46ca8cc796281a9c6e17e6eb90919441e0edb7
Jianwei <er891129@gmial.com>
2025-07-14 14:36:28 +0800

    聲音bug修

--- all.js
+++ all.js
@@ -1,12 +1,15 @@
-// 假裝5秒後出現 Jump Scare
-setTimeout(() => {
-  const jumpscare = document.getElementById("jumpscare");
-  jumpscare.style.display = "flex";
-}, 5000); // 5秒後驚嚇
 
-setTimeout(()=>{
-    const scream = document.getElementById("scream");
-    const joker = document.querySelector(".jumpscare img");
-    joker.classList.add("show");
-    scream.play();
-},6000)
+function startScare() {
+  document.body.onclick = null; // 避免重複觸發
+  setTimeout(() => {
+    const jumpscare = document.getElementById("jumpscare");
+    jumpscare.style.display = "flex";
+  }, 5000); // 5秒後驚嚇
+  
+  setTimeout(()=>{
+      const scream = document.getElementById("scream");
+      const joker = document.querySelector(".jumpscare img");
+      joker.classList.add("show");
+      scream.play();
+  },6000)
+  }
