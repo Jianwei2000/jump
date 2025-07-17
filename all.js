d74b87c2305130ecad8d60caf588b2ec253ab8ac
Jianwei <er891129@gmial.com>
2025-07-17 15:50:50 +0800

    update

--- all.js
+++ all.js
@@ -5,6 +5,7 @@ function startScare() {
     const jumpscare = document.getElementById("jumpscare");
     jumpscare.style.display = "flex";
     const laugh = document.getElementById("laugh");
+    laugh.volume = 1.0;
     laugh.play();
   }, 2000); // 2秒後驚嚇
   
